310f19c9f40f8594fa57c5d2edca0425ec1a1718
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2023-01-26 23:54:01 +0000

    Test Setup

---
+++ Chap5TestQuest3.py
@@ -0,0 +1,5 @@
+grades = {'A+':96, 'A':94, 'A-':91, 'B+':87, 'B':85, 'B-':82, 'C+':78, 'C':74, 'C-':71, 'D':65, 'F':60}
+
+"""
+    Using the grades dictionary, print out each key: value, all on separate lines.
+"""
---
+++ Chpt5TestQuest2.py
@@ -0,0 +1,21 @@
+"""
+    Dictionary Functions and Operators
+    Use the Dictionary to follow the steps
+    listed in the notes of the application.
+"""
+dict1 = {'a':284, 'b':132, 'c':64, 'e':430, 'i':189, 'o':132, 'r':189, 's':148, 't':165}
+
+# Print out the length of the list
+
+
+# Check to see if the letter r is in the 
+# list, if it is, print out it's value. If it 
+# isn't, print "The letter r is not in the 
+# dictionary"
+
+
+# Remove the letter b and it's value from
+# the dictionary
+
+
+# Change the value of the letter 'o' to 178 and print out the full dictionary
