b40b82d32409fb18485102372640ecbe465a2ee1
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2023-01-26 15:59:42 +0000

    Test Setup

---
+++ Chpt5TestQuest1.py
@@ -0,0 +1,25 @@
+"""
+File: computesquare.py
+Calculates the square of a number entered by the user. 
+"""
+
+number = float(input("Enter a number: "))
+result = number * number
+print("The square of", number, "is", result)
+
+"""
+    Using the code above, reprogram this app to use 2 functions, one called main(),
+    one called square(). Also use a conditional statement to launch the application
+"""
+
+# Create the main() function: this will collect the user input and then calls the
+# square() function.
+
+
+# Create the square() function: This will take the argument from the function call
+# and use it to calculate the square. Make sure you return the value of the square
+# back to the main function.
+
+
+# Create the conditional statement as the entry point for the application. HINT an if
+# statement that calls the main() function.
